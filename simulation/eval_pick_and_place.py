33a6011d009f2493c511545a1ef0c49af737c394
daihangPKU <daihang2300012956@163.com>
2025-12-09 14:27:12 +0800

    eval

--- simulation/eval_pick_and_place.py
+++ simulation/eval_pick_and_place.py
@@ -10,10 +10,8 @@ sys.path.insert(0, os.getcwd())
 import genesis as gs
 import yaml
 from pathlib import Path
-from easydict import EasyDict
-from simulation.utils.constants import BEST_PARAMS, JOINT_NAMES
-from simulation.utils.auto_collect.franka_genesis_controller import pick_and_place_controller
-from simulation.auto_collect_pick_and_place import design_scene as design_pnp_scene
+from simulation.controller.franka_genesis_controller import pick_and_place_controller
+from simulation.utils.scene_utils import design_pnp_scene, init_scene_physic_params
 from simulation.render_pick_and_place import render_scene
 from termcolor import cprint
 from tqdm import tqdm
@@ -31,40 +29,10 @@ except:
     ROSPY_ENABLED = False
     raise NotImplementedError
         
-def rotate_axis_quaternion(ori_axis):
-    if ori_axis == 'x':
-        return (np.sqrt(2) / 2, 0, -np.sqrt(2) / 2, 0)
-    elif ori_axis == 'y':
-        return (np.sqrt(2) / 2, np.sqrt(2) / 2, 0, 0)
-    elif ori_axis == 'z':
-        return (1, 0, 0, 0)
-    elif ori_axis == "-x":
-        return (np.sqrt(2) / 2, 0, np.sqrt(2) / 2, 0)
-    elif ori_axis == "-y":
-        return (np.sqrt(2) / 2, -np.sqrt(2) / 2, 0, 0)
-    elif ori_axis == "-z":
-        return (0, 1, 0, 0)
-    else:
-        return (1, 0, 0, 0)
 
-def get_object_bbox_and_height(object_ply, ori_axis):
-    ply_o3d = o3d.io.read_triangle_mesh(object_ply)
-    bbox = ply_o3d.get_axis_aligned_bounding_box()
-    bbox_minbound = bbox.get_min_bound()
-    bbox_maxbound = bbox.get_max_bound()
-    if "x" in ori_axis:
-        height = (bbox_maxbound[0] - bbox_minbound[0])
-    elif "y" in ori_axis:
-        height = (bbox_maxbound[1] - bbox_minbound[1])
-    elif "z" in ori_axis:
-        height = (bbox_maxbound[2] - bbox_minbound[2])
-    else:
-        raise NotImplementedError(f"unknown axis {ori_axis}")
-    return bbox, height
-
-def design_scene(scene_config, show_viewer=False):
-    scene, scene_config, scene_dict, scene_asset_path_dict, grasp_cam, default_poses = design_pnp_scene(scene_config, show_viewer=show_viewer)
-    return scene, scene_config, scene_dict, scene_asset_path_dict, grasp_cam, default_poses
+def design_scene(scene_config, robot_config, show_viewer=False):
+    scene, scene_dict, cams, default_poses = design_pnp_scene(scene_config, robot_config, show_viewer=show_viewer)
+    return scene, scene_dict, cams, default_poses
 
 image_queue = queue.Queue(maxsize=1) 
 
@@ -96,14 +64,14 @@ def consumer_thread():
         cv2.destroyAllWindows()
         
 def main(args):
-    scene_config = EasyDict(yaml.safe_load(Path(args.cfg_path).open('r')))
-    render_types = ["rgb"]
+    scene_config = yaml.safe_load(Path(args.scene_cfg_path).open('r'))
+    robot_config = yaml.safe_load(Path(args.robot_cfg_path).open('r'))
     
     from datetime import datetime
     now = datetime.now()
     milliseconds = now.microsecond // 1000
     timestamp = now.strftime("%Y%m%d_%H%M%S") + f"_{milliseconds:03d}"
-    task_name = scene_config.task_name
+    task_name = scene_config["task_name"]
     process_output_dir = os.path.join(args.output_dir, task_name, timestamp)
     os.makedirs(process_output_dir, exist_ok=True)
                              
@@ -113,7 +81,7 @@ def main(args):
     
     # Init Genesis
     gs.init(backend=gs.gpu, logging_level = 'warning')
-    scene, scene_config, scene_dict, scene_asset_path_dict, grasp_cam, default_poses = design_scene(scene_config, show_viewer=False)
+    scene, scene_dict, cams, default_poses = design_scene(scene_config, robot_config, show_viewer=False)
     scene.build()
     
 
@@ -121,25 +89,23 @@ def main(args):
     robot = scene_dict["robot"]
     object_active = scene_dict["object_active"]
     object_passive = scene_dict["object_passive"]
-    
-    # Set phys params
-    all_dof_ids = [robot.get_joint(name).dof_idx for name in JOINT_NAMES]
-    robot.set_dofs_kp(kp = BEST_PARAMS["kp"], dofs_idx_local=all_dof_ids[:7])
-    robot.set_dofs_kv(kv = BEST_PARAMS["kv"], dofs_idx_local=all_dof_ids[:7])
-    robot.set_dofs_kp(kp = [50000, 50000], dofs_idx_local=all_dof_ids[7:9])
-    robot.set_dofs_kv(kv = [10000, 10000], dofs_idx_local=all_dof_ids[7:9])
-    robot.set_dofs_force_range([-100, -100], [100, 100], dofs_idx_local=all_dof_ids[7:9])
-    
-    object_active.get_link("object").set_mass(0.1)
-    object_active.get_link("object").set_friction(0.2)
+    init_scene_physic_params(scene, scene_dict, scene_config, robot_config)
 
     cprint("*" * 40, "green")
     cprint("  Initializing Controller", "green")
     cprint("*" * 40, "green")
     
     # Init Controller
-    controller = pick_and_place_controller(scene=scene, scene_config=scene_config, robot=robot, object_active=object_active, object_passive=object_passive, default_poses=default_poses, close_thres=scene_config.robot.close_thres, teleop=False, evaluation=True)
-    
+    controller = pick_and_place_controller(
+        scene=scene,
+        scene_dict=scene_dict,
+        scene_config=scene_config,
+        robot_config=robot_config,
+        default_poses=default_poses,
+        close_thres=robot_config["close_thres"],
+        teleop=False,
+        evaluation=True
+    )
     def reset_layout():
         # Reset Layout
         while True:
@@ -149,19 +115,22 @@ def main(args):
             passive_aabb = object_passive.get_link("object").get_AABB()
             active_aabb = object_active.get_link("object").get_AABB()
             
-            # object aabb must in the borders
-            if active_aabb[0, 0] > scene_config.object_active.pos_range.x[0] / 100 and \
-               active_aabb[0, 1] > scene_config.object_active.pos_range.y[0] / 100 and \
-               active_aabb[1, 0] < scene_config.object_active.pos_range.x[1] / 100 and \
-               active_aabb[1, 1] < scene_config.object_active.pos_range.y[1] / 100 :
+            if (
+                active_aabb[0, 0] > scene_config["object_active"]["pos_range"]["x"][0] / 100
+                and active_aabb[0, 1] > scene_config["object_active"]["pos_range"]["y"][0] / 100
+                and active_aabb[1, 0] < scene_config["object_active"]["pos_range"]["x"][1] / 100
+                and active_aabb[1, 1] < scene_config["object_active"]["pos_range"]["y"][1] / 100
+            ):
                 pass
             else:
                 cprint("active out of range", "yellow")
                 continue
-            if passive_aabb[0, 0] > scene_config.object_passive.pos_range.x[0] / 100 and \
-               passive_aabb[0, 1] > scene_config.object_passive.pos_range.y[0] / 100 and \
-               passive_aabb[1, 0] < scene_config.object_passive.pos_range.x[1] / 100 and \
-               passive_aabb[1, 1] < scene_config.object_passive.pos_range.y[1] / 100 :
+            if (
+                passive_aabb[0, 0] > scene_config["object_passive"]["pos_range"]["x"][0] / 100
+                and passive_aabb[0, 1] > scene_config["object_passive"]["pos_range"]["y"][0] / 100
+                and passive_aabb[1, 0] < scene_config["object_passive"]["pos_range"]["x"][1] / 100
+                and passive_aabb[1, 1] < scene_config["object_passive"]["pos_range"]["y"][1] / 100
+            ):
                 pass
             else:
                 cprint("passive out of range", "yellow")
@@ -173,7 +142,6 @@ def main(args):
             if x_overlap and y_overlap:
                 cprint("active-passive overlap box", "yellow")
                 continue
-            # the objects should not be too near
             if np.linalg.norm(passive_pos - active_pos) > scene_config.far_threshold:
                 cprint("active-passive too near", "yellow")
                 break
@@ -198,13 +166,13 @@ def main(args):
     consumer = threading.Thread(target=consumer_thread, daemon=True)
     consumer.start()
     
-    pub_image = rospy.Publisher('/twinmanip/color_image', Image, queue_size=1)
+    pub_image = rospy.Publisher('/genesis/color_image', Image, queue_size=1)
     
     while True:
         if not listener.is_alive():
             listener = keyboard.Listener(on_press=on_press, on_release=on_release)
             listener.start()
-        rgb_image, depth_image = render_scene(scene_dict, None, grasp_cam, args, render_types=render_types)
+        rgb_image, depth_image = render_scene(scene, scene_dict, None, cams["desk_cam"])
         
         if not image_queue.empty():
             try:
@@ -230,8 +198,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--output_dir", type=str, default="datasets/records")
-    parser.add_argument("--cfg_path", type=str, default="simulation/configs/banana_plate.yaml")
-    parser.add_argument("--depth_only", type=bool, default=False)
-    parser.add_argument("--rgb_only", type=bool, default=True)
+    parser.add_argument("--scene_cfg_path", type=str, default="simulation/configs/scene/banana_plate.yaml")
+    parser.add_argument("--robot_cfg_path", type=str, default="simulation/configs/robot/panda_xml.yaml")
     args = parser.parse_args()
     main(args)
